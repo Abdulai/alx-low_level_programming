4250d0a2f687ad1a4d62b08e0701b44bed8193ce
Abdulai <abdulbah40@hotmail.co.uk>
2023-02-16 08:01:37 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,20 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("size of a char: %lu byte(s)\n" (unsigned long) sizeof(a));
+printf("size of a char: %lu byte(s)\n" (unsigned long) sizeof(b));
+printf("size of a char: %lu byte(s)\n" (unsigned long) sizeof(c));
+printf("size of a char: %lu byte(s)\n" (unsigned long) sizeof(d));
+printf("size of a char: %lu byte(s)\n" (unsigned long) sizeof(e));
+return (0);
+}
