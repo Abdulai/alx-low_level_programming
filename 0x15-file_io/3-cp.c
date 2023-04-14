df5b7ae8028c699802727a5df78d010360b611e6
Abdulai <abdulbah40@hotmail.co.uk>
2023-04-14 09:06:48 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -39,7 +39,7 @@ void close_file(int fd)
 
 	if (c == -1)
 	{
-		drintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
